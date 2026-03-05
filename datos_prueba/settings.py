924fa038084cd0d6d81fe1521418bed18aa09ffc
Robert Napier <napierfamilymallorcarafn@gmail.com>
2026-03-05 12:40:01 +0100

    Añadido favicon, ajustes en settings y personalización del admin

--- datos_prueba/settings.py
+++ datos_prueba/settings.py
@@ -139,10 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'productionstaticfiles'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR /'datos_prueba/globalstaticfiles',
-    BASE_DIR /'muestras/static'
 ]
 
 # Default primary key field type
